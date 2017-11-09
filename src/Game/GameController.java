c99ac2d260a4141c47047bbf2d875b4db88b2a24
oliwex <oliwexminio@gmail.com>
2017-11-09 09:17:45 +0100

    First commit

---
+++ src/Game/GameController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author oliwe
+ */
+public class GameController implements Initializable 
+{
+
+
+  //  private Image cpuSign=new Image("/image/CPU.png");
+   /// private Image playerSign=new Image("/image/PLAYER.png");
+    
+  //  private BorderPane borderPane=new BorderPane();
+    
+    
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+}
---
+++ src/TicTacToe.java
@@ -0,0 +1,50 @@
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+
+import javafx.scene.layout.BorderPane;
+
+
+import javafx.stage.Stage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author oliwe
+ */
+public class TicTacToe extends Application {
+    
+
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        try {
+            FXMLLoader loader=new FXMLLoader(this.getClass().getResource("/Game/Game.fxml"));
+            BorderPane borderPane=loader.load();
+            Scene scene=new Scene(borderPane);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("TicTacToe Game");
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(TicTacToe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
